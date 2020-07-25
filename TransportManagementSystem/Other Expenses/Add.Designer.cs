e3cb691417a82ca1f3644a8b19ef7fd898f75453
chhatraman shrestha <chhatramanshrestha@outlook.com>
2020-07-25 21:32:09 +0545

    textbox added

--- TransportManagementSystem/Other Expenses/Add.Designer.cs
+++ TransportManagementSystem/Other Expenses/Add.Designer.cs
@@ -28,12 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.betterTextBox1 = new CustomControls.BetterTextBox();
+            this.SuspendLayout();
+            // 
+            // betterTextBox1
+            // 
+            this.betterTextBox1.decVal = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.betterTextBox1.intVal = 0;
+            this.betterTextBox1.Location = new System.Drawing.Point(588, 159);
+            this.betterTextBox1.Name = "betterTextBox1";
+            this.betterTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.betterTextBox1.TabIndex = 0;
+            this.betterTextBox1.textboxType = CustomControls.TextboxType.Text;
+            // 
+            // Add
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.betterTextBox1);
+            this.Name = "Add";
             this.Text = "Add";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private CustomControls.BetterTextBox betterTextBox1;
     }
 }
